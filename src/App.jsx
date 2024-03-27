20371051739aca1987d7c227f369eb3dd8031887
PITT1 <petroleo.plta.97@gmail.com>
2024-03-27 17:54:39 -0400

    app lista pero solo con single jokes

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Button from './components/Button'
 import JokeArea from './components/JokeArea'
 
-let tipeJoke = []
-let setupJoke = []
-let deliveryJoke = []
-let singleJoke = []
-
 function App() {
+  const [isSingle, setIsSingle] = useState(false);
+  const [joke, setJoke] = useState("");
 
   const callJokes = () => {
+    fetch("https://v2.jokeapi.dev/joke/Any?lang=es&type=single")
+      .then(res => res.json())
+      .then(data => {
+        setJoke(data.joke);
+        setIsSingle(true);
+        console.log(joke);
+      })
+      .catch(error => console.error('Error fetching joke:', error));
+ };
 
-  }
-
-  useEffect(() => {
-    fetch("https://v2.jokeapi.dev/joke/Any?lang=es&blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=10")
-    .then(res => res.json())
-    .then(data => {
-
-      for (let i = 0; i < data.jokes.length; i++) {
-        if(data.jokes[i].type == "single" ){
-          tipeJoke.push(data.jokes[i].type);
-          singleJoke.push(data.jokes[i].joke);
-        } else if(data.jokes[i].type == "twopart"){
-          tipeJoke.push(data.jokes[i].type);
-          setupJoke.push(data.jokes[i].setup);
-          deliveryJoke.push(data.jokes[i].delivery);
-        }
-      }
-
-    })
-
-    return () => {
-
-    }
-  },[]);
+ useEffect(() => {
+  setJoke(joke)
+ },[joke]);
 
   return (
     <>
     <Button text={"cuentame un chiste"} fun={callJokes}/>
-    <JokeArea 
-      text1='que le dice un pollito policia a otro pollito policia'
-      text2={"necesitamos apollo"}
-      typeJoke={"twopart"}/>
+    {isSingle && <JokeArea text={joke}/>}
     </>
   )
 }
--- src/components/JokeArea.jsx
+++ src/components/JokeArea.jsx
@@ -1,28 +1,16 @@
 import propTypes from 'prop-types'
 import '../App.css'
 
-const JokeArea = ({text1, text2, typeJoke}) => {
-
-    if(typeJoke == "single"){
-        return(
-            <section className='section-joke'>
-                <div className='text-joke'>{text1}</div>
-            </section>
-        )
-    } else if(typeJoke == "twopart"){
-        return(
-            <section className='section-joke'>
-                <div className='text-joke'>{text1}</div>
-                <div className='text-joke'>{text2}</div>
-            </section>
-        )
-    }
+const JokeArea = ({text}) => {
+    return(
+        <section className='section-joke'>
+            <div className='text-joke'>{text}</div>
+        </section>
+    )
 }
 
 export default JokeArea
 
 JokeArea.propTypes = {
-    text1: propTypes.string.isRequired,
-    text2: propTypes.string,
-    typeJoke: propTypes.string
+    text: propTypes.string.isRequired
 }
